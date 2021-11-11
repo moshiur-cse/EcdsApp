190c41792513cff47d6ec7e94cd2bad11dcc8d12
Moshiur Rahman <moshiur_cse@hotmail.com>
2021-11-11 15:06:04 +0600

    update Map

---
+++ Controllers/Map/MapController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcdsApp.Controllers.Map
+{
+    public class MapController : Controller
+    {
+        public IActionResult BasicMap()
+        {
+            return View();
+        }
+        public IActionResult ThematicMap()
+        {
+            return View();
+        }
+    }
+}
