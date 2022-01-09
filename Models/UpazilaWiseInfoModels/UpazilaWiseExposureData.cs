ed72f76dc09afa5eba68079f3eef88a0c29aa0ed
Moshiur Rahman <moshiur_cse@hotmail.com>
2022-01-09 10:10:21 +0600

    Update Models

--- Models/UpazilaWiseInfoModels/UpazilaWiseExposureData.cs
+++ Models/UpazilaWiseInfoModels/UpazilaWiseExposureData.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace EcdsApp.Models.UpazilaWiseInfoModels
 {
+    [Table("tbl_upazila_wise_exposure_data")]
     public class UpazilaWiseExposureData
     {
         [Key]
