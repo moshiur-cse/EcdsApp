87b0cbe57e7870e0f89fdf4d4eafaa4a43b7e1c7
Abdullah Ibn Mannan <sabbir.mannan@gmail.com>
2022-01-25 17:42:29 +0600

    dashboard-dynamic-graph

---
+++ Models/ViewModels/Dashboard/DashboardVm.cs
@@ -0,0 +1,12 @@
+﻿using EcdsApp.Models.ThemeModels;
+using EcdsApp.Models.UpazilaWiseInfoModels;
+using System.Collections.Generic;
+
+namespace EcdsApp.Models.ViewModels.Dashboard
+{
+    public class DashboardVm
+    {
+        public IEnumerable<UpazilaWiseExposureData> UpazilaWiseExposureData { get; set; }
+        public IEnumerable<ThemeLayerDetail> ThemeLayerDetails { get; set; }
+    }
+}
