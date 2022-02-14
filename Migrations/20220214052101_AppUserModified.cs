b1ce05f40cb5e836d4ecbb2e196720e37aecb41f
abdullahmannan <sabbir.mannan@gmail.com>
2022-02-14 16:45:39 +0600

    user-authentication

---
+++ Migrations/20220214052101_AppUserModified.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EcdsApp.Migrations
+{
+    public partial class AppUserModified : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "address",
+                table: "users",
+                type: "varchar(500)",
+                maxLength: 500,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "date_of_birth",
+                table: "users",
+                type: "varchar(20)",
+                maxLength: 20,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "designation",
+                table: "users",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "first_name",
+                table: "users",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "full_name",
+                table: "users",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "last_name",
+                table: "users",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "mobile_no",
+                table: "users",
+                type: "varchar(11)",
+                maxLength: 11,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "organization",
+                table: "users",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "address",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "date_of_birth",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "designation",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "first_name",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "full_name",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "last_name",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "mobile_no",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "organization",
+                table: "users");
+        }
+    }
+}
--- Models/UserManage/ApplicationUser.cs
+++ Models/UserManage/ApplicationUser.cs
@@ -8,6 +8,46 @@ namespace EcdsApp.Models.UserManage
     [Table("users")]
     public class ApplicationUser : IdentityUser
     {
+        [Column("first_name")]
+        [StringLength(50)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Column("last_name")]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Display(Name = "Full Name")]
+        [Column("full_name")]
+        [StringLength(100)]
+        public string FullName { get; set; }
+
+        [Display(Name = "Date Of Birth")]
+        [Column("date_of_birth")]
+        [StringLength(20)]
+        public string DateOfBirth { get; set; }
+
+        [Display(Name = "Address")]
+        [Column("address")]
+        [StringLength(500)]
+        public string Address { get; set; }
+
+        [Display(Name = "Mobile No")]
+        [Column("mobile_no")]
+        [StringLength(11)]
+        public string MobileNo { get; set; }
+
+        [Display(Name = "Organization")]
+        [Column("organization")]
+        [StringLength(100)]
+        public string Organization { get; set; }
+
+        [Display(Name = "Designation")]
+        [Column("designation")]
+        [StringLength(50)]
+        public string Designation { get; set; }
+
         //[Required]
         [Column("user_activation_status_id")]
         [Display(Name = "User Activation Status Id")]
