9286c33b7688a11929a7ff83a592dfde0deec4a7
Moshiur Rahman <moshiur_cse@hotmail.com>
2022-03-29 14:17:12 +0600

    Update .net 5 to .net 6

---
+++ Migrations/20220329081031_UpdateLegend.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EcdsApp.Migrations
+{
+    public partial class UpdateLegend : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "purpose_of_production",
+                table: "tbl_metadata_details",
+                type: "varchar(1000)",
+                maxLength: 1000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(1000)",
+                oldMaxLength: 1000);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "general",
+                table: "tbl_metadata_details",
+                type: "varchar(1000)",
+                maxLength: 1000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(1000)",
+                oldMaxLength: 1000);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "completeness",
+                table: "tbl_metadata_details",
+                type: "varchar(1000)",
+                maxLength: 1000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(1000)",
+                oldMaxLength: 1000);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "layer_legend_color_code",
+                table: "tbl_layer_legend_colors",
+                type: "varchar(10)",
+                maxLength: 10,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(10)",
+                oldMaxLength: 10);
+
+            migrationBuilder.AddColumn<string>(
+                name: "icon_path",
+                table: "tbl_layer_legend_colors",
+                type: "varchar(200)",
+                maxLength: 200,
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "icon_size",
+                table: "tbl_layer_legend_colors",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "icon_path",
+                table: "tbl_layer_legend_colors");
+
+            migrationBuilder.DropColumn(
+                name: "icon_size",
+                table: "tbl_layer_legend_colors");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "purpose_of_production",
+                table: "tbl_metadata_details",
+                type: "varchar(1000)",
+                maxLength: 1000,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(1000)",
+                oldMaxLength: 1000,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "general",
+                table: "tbl_metadata_details",
+                type: "varchar(1000)",
+                maxLength: 1000,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(1000)",
+                oldMaxLength: 1000,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "completeness",
+                table: "tbl_metadata_details",
+                type: "varchar(1000)",
+                maxLength: 1000,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(1000)",
+                oldMaxLength: 1000,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "layer_legend_color_code",
+                table: "tbl_layer_legend_colors",
+                type: "varchar(10)",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(10)",
+                oldMaxLength: 10,
+                oldNullable: true);
+        }
+    }
+}
