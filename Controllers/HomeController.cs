470a6a17bbbfb1a1200079e292cc7c4188df423a
Abrar Zahin <Abrar.bd27@gmail.com>
2022-04-18 00:52:45 +0600

    Changed url link and name for contact us page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -143,7 +143,7 @@ namespace EcdsApp.Controllers
         //    return View();
         //}
 
-        public IActionResult UserFeedback()
+        public IActionResult ContactUs()
         {
             return View();
         }
