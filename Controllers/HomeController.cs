e8f484ef7397c96d93d05efcc70030d78de4bfeb
Abrar Zahin <Abrar.bd27@gmail.com>
2022-04-28 14:25:28 +0600

    removed unnecessary comments

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -161,32 +161,6 @@ namespace EcdsApp.Controllers
             return View();
         }
 
-        //public async Task Login()
-        //{
-        //    await HttpContext.ChallengeAsync(GoogleDefaults.AuthenticationScheme, new AuthenticationProperties()
-        //    {
-        //        RedirectUri = Url.Action("GoogleResponse")
-        //    });
-        //}
-
-        //public async Task<IActionResult> GoogleResponse()
-        //{
-        //    var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-        //    var claims = result.Principal.Identities.FirstOrDefault().Claims.Select(clm => new
-        //    {
-        //        clm.Issuer,
-        //        clm.OriginalIssuer,
-        //        clm.Type,
-        //        clm.Value
-        //    });
-        //    return Json(claims);
-        //}
-
-        //public async Task<IActionResult> Logout()
-        //{
-        //    await HttpContext.SignOutAsync();
-        //    return RedirectToAction("Dashboard");
-        //}
 
     }
 }
