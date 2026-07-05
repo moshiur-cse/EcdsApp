d93046cbc706df797952bf1e32355a0d14446c12
Moshiur Rahman Rimu <moshiur.mgmh@gmail.com>
2026-07-05 22:38:30 +1200

    change credential

--- Data/Credentials.cs
+++ Data/Credentials.cs
@@ -5,14 +5,14 @@ namespace DRIPWebApp.Data
 {
     public class Credentials
     {
-        //public static string Host = "mail.cegisbd.com";
+        //public static string Host = "mail.cegiswebsitename.com";
         //public static int Port = 25;
         //public static string EmailSender = "test@cegisbd.com";
         //public static string UserName = "test";
         //public static string Password = "cegis@V25";
         //public static string DefaultEmailReceiver = "test@cegisbd.com";
 
-        public static string Host = "mail.bbs.gov.bd";
+        public static string Host = "mail.bbswebsitename.gov.bd";
         public static int Port = 25;
         public static string EmailSender = "admin_ecds@bbs.gov.bd";
         public static string UserName = "admin_ecds@bbs.gov.bd";
